b902bdf989922e39724df31ab3d7025dc37e8b7c
akshatas28 <akshata.saptasagar@gmail.com>
2026-07-01 23:43:01 +0530

    Add Jarvis Virtual Assistant with speech recognition, text-to-speech, wake-word detection, and web automation

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music={
+    "heat":"https://youtu.be/-NqnPNV-llk?list=RD-NqnPNV-llk",
+    "boy":"https://youtu.be/34Na4j8AVgA",
+    "live":"https://youtu.be/Xv-VtFaEwPo",
+    "sky":"https://youtu.be/sZrTJesvJeo?list=RDsZrTJesvJeo"
+}   
